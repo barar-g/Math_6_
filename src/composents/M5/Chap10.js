dc8aafc75aeef4d9cca5543fc992a9680d0ca648
ahmedoutayeb <ahmedoutayeb78@gmail.com>
2023-11-09 18:41:40 +0000

    Your commit message here

--- src/composents/M5/Chap10.js
+++ src/composents/M5/Chap10.js
@@ -49,7 +49,6 @@ function CalculateSquareArea() {
             setShowCongrats(true);
         }else{
             setShowCongrats(false);
-            setOpverify(false);
             setShowX(true);
            
             setTimeout(() => {
